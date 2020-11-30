d4df50e80c9d44240fca86148f1c6db23e3cb161
gmoraitis <moraitis.georgios@icloud.com>
2020-11-30 22:56:19 +0200

    Added contact information

--- js/game.js
+++ js/game.js
@@ -13,15 +13,15 @@ function play_pause() {
 window.addEventListener("load", () => {
   //window loaded
   //do something
-  
+
   document.addEventListener("keypress", function (event) {
     var node = document.createElement("LI");
     if (event.key === "Enter") {
       var text = document.getElementById("input").value.toLowerCase();
-      
+
       // Start
       if (text === 'start') {
-        
+
         var textnode = document.createTextNode("The year is 2789. Humankind had leave planet Earth a lot of time ago. They found peace on a planet name Nexus far far away from Earth. After a long time, a bad creature named Darun from planet Ratmond decided to kill all humans and conquest planet Nexus. You are in the middle of this war and you are about to save mankind. You found an old map for a secret room from an old forgotten wizard that will guide you to find the code to deactivate Darun's bomb. You are in this room, type north , east, west, south to move in the room. Only type the word...");
         node.appendChild(textnode);
         document.getElementById("computer").appendChild(node);
@@ -30,7 +30,7 @@ window.addEventListener("load", () => {
       }
       // North
       else if (text === "north") {
-        
+
         var textnode = document.createTextNode("There is a wall in front of you with some old pictures and dust books. I don't think that you will find something there...Try something else...   ");
         node.appendChild(textnode);
         document.getElementById("computer").appendChild(node);
@@ -38,7 +38,7 @@ window.addEventListener("load", () => {
 
       // East
       else if (text === "east") {
-    
+
         var textnode = document.createTextNode("Also nothing here but hey look carefully there is a bottle with some special magic juice. Some of the wizard's old tricks...Drink it to get power to continue, and try something else...");
         node.appendChild(textnode);
         document.getElementById("computer").appendChild(node);
@@ -46,7 +46,7 @@ window.addEventListener("load", () => {
 
       // West
       else if (text === "west") {
-        
+
         var textnode = document.createTextNode("Again nothing here but wait ... there is an old note and it might be from the wizard...Type 'read note' to read it...There must be a clue inside right?");
         node.appendChild(textnode);
         document.getElementById("computer").appendChild(node);
@@ -54,7 +54,7 @@ window.addEventListener("load", () => {
 
       // Open Note
       else if (text === "read note") {
-        
+
         var textnode = document.createTextNode(" ...Yes I am the wizard and you did great so far...There is one place that didn't go...Can you imagine what it is?... You will face 3 challenges when you find what you are looking for... ");
         node.appendChild(textnode);
         document.getElementById("computer").appendChild(node);
