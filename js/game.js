da6b2831e55241602e3f81950ce193917958e1fe
gmoraitis <moraitis.georgios@icloud.com>
2020-11-30 18:15:18 +0200

    Changes on title and theme of project

---
+++ js/game.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", () => {
+  //window loaded
+  //do something
+
+  document.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      var text = document.getElementById("input").value;
+    
+
+      if (text === "north") {
+        // document.getElementById("computer").appendChild("r \ ");
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode("You went north");
+        node.appendChild(textnode);
+        document.getElementById("computer").appendChild(node);
+     
+      }
+      console.log(text);
+      if (text === "right") {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode("You went east");
+        node.appendChild(textnode);
+        document.getElementById("computer").appendChild(node);
+      }
+    }
+  });
+});
